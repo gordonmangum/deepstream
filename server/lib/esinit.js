10086e9031a5d8a953113a40ff33c5f68033bc4f
Abdulrahman Alotaibi <adminq80@gmail.com>
2015-10-26 14:18:04 -0400

    Wrapped the init code in a function and added a filter for synonyms

--- server/lib/esinit.js
+++ server/lib/esinit.js
@@ -1,11 +1,14 @@
 
-ES = Meteor.npmRequire('elasticsearch');
-esClient = new ES.Client({
+esClient = function initES(){
+var ES = Meteor.npmRequire('elasticsearch');
+var esClient = new ES.Client({
                  host: process.env.ELASTICSEARCH_URL || Meteor.settings.ELASTICSEARCH_URL || "localhost:9200"
 });
 
 
-esClient.ping({
+var ping = Meteor.wrapAsync(esClient.ping, esClient);
+
+ping({
   // ping usually has a 3000ms timeout
   requestTimeout: Infinity,
 
@@ -25,38 +28,86 @@ var putMapping = Meteor.wrapAsync(esClient.indices.putMapping, esClient);
 var putSettings = Meteor.wrapAsync(esClient.indices.putSettings, esClient);
 var closeIndex = Meteor.wrapAsync(esClient.indices.close, esClient);
 var openIndex = Meteor.wrapAsync(esClient.indices.open, esClient);
-
+var deleteData = Meteor.wrapAsync(esClient.delete, esClient);
 
 function esCreateMapping(json, callback){
 	putMapping(json, callback);
 }
 
+// if (process.env.NODE_ENV === 'development' && indexExists({index: ES_CONSTANTS.index}) ){
+//   console.log("Going to cleanup ES");
+//     deleteData({
+//       index: ES_CONSTANTS.index,
+//       type: "stream",
+//       body: {
+//         "query":{
+//           "match_all": {}
+//         }
+//       }
+//     },function(e, r){
+//       if (e)
+//       console.log(e);
+//       else {
+//         console.log("Deleted all ES data");
+//       }
+//     });
+//   }
+
 if (!indexExists({index: ES_CONSTANTS.index})){
-	 createIndex({ index: ES_CONSTANTS.index });
+  console.log("indexExists function: going to create");
+	 createIndex({index: ES_CONSTANTS.index });
    Meteor._sleepForMs(1000);
    closeIndex({index: ES_CONSTANTS.index});
+   console.log("indexExists function: going to close index");
+
+   console.log("indexExists function: going to apply settings");
 
    putSettings({
               index: ES_CONSTANTS.index,
               type: "stream",
-              "body":{
-                  "analysis": {
-                    "analyzer": {
-                      "my_ngram_analyzer": {
-                        "tokenizer": "my_ngram_tokenizer",
-                        "filter": ["standard", "lowercase"],
+              body: {
+                "analysis" : {
+                  "analyzer" : {
+                    "my_ngram_analyzer" : {
+                      "tokenizer" : "my_ngram_tokenizer",
+                      "filter" : ["standard", "lowercase", "synonym",]
                     }
                   },
-                    "tokenizer": {
-                      "my_ngram_tokenizer":{
-                        "type": "nGram",
-                        "min_gram": 2,
-                        "max_gram": 7,
-                        "token_chars": ["letter", "digit"],
-                      }
+                  "tokenizer": {
+                    "my_ngram_tokenizer":{
+                      "type": "nGram",
+                      "min_gram": 2,
+                      "max_gram": 15,
+                      "token_chars": ["letter", "digit"],
+                  }
+                },
+                  "filter" : {
+                    "synonym" : {
+                      "type" : "synonym",
+                      "synonyms_path" : "analysis/wn_s.pl"
                     }
                   }
                 }
+              }
+
+              // "body":{
+              //     "analysis": {
+              //       "analyzer": {
+              //         "my_ngram_analyzer": {
+              //           "tokenizer": "my_ngram_tokenizer",
+              //           "filter": ["standard", "lowercase"]
+              //       }
+              //     },
+              //       "tokenizer": {
+              //         "my_ngram_tokenizer":{
+              //           "type": "nGram",
+              //           "min_gram": 2,
+              //           "max_gram": 15,
+              //           "token_chars": ["letter", "digit"]
+              //         }
+              //       }
+              //     }
+              //   }
             }, function(err, result){
               if(err){
                 console.log("ElasticSearch: putSettings: Error");
@@ -68,6 +119,8 @@ if (!indexExists({index: ES_CONSTANTS.index})){
               }
             });
       Meteor._sleepForMs(1000);
+      console.log("indexExists function: going to apply mappings");
+
       esCreateMapping({
               index: ES_CONSTANTS.index,
               type: "stream",
@@ -112,3 +165,6 @@ if (!indexExists({index: ES_CONSTANTS.index})){
             });
     openIndex({index: ES_CONSTANTS.index});
   }
+
+return esClient;
+}();
