d1e9cbc97a7323d9b9204d081a50e83367748e6b
Joseph Goldbeck <goldbeck.joseph@gmail.com>
2015-10-29 21:51:46 -0400

    https redirect to http

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 // If use ssl, will need to check that too
 // TO-DO change this to a 301 redirect once totally sure
 WebApp.connectHandlers.use(function(req, res, next) {
-  if (req.method === 'GET' && req.headers.host.match(/^www/) !== null ) {
+  if (req.method === 'GET' && req.headers.host.match(/^www/) !== null ) { // www redirect to bare domain
     res.writeHead(307, {Location: req.headers['x-forwarded-proto'] + '://' + req.headers.host.replace(/^www\./, '') + req.url});
+    res.end();
+  } else if (req.method === 'GET' && req.headers['x-forwarded-proto'].match(/^https/) !== null ) { // https redirect to http
+    res.writeHead(307, {Location: 'http://' + req.headers.host + req.url});
     res.end();
   } else {
     next();
