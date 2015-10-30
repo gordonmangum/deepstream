d93887d7fe248c40c0f76557a741c86e054a95e5
Joseph Goldbeck <goldbeck.joseph@gmail.com>
2015-10-29 22:02:50 -0400

    add route for comodo authentication

--- server/index.js
+++ server/index.js
@@ -12,6 +12,17 @@ WebApp.connectHandlers.use(function(req, res, next) {
   }
 });
 
+// for comodo authentication TO-DO Remove
+WebApp.connectHandlers.use(function(req, res, next) {
+  if (req.method === 'GET' && req.url === '/1BAE280053CE3BEF6AEC2829F100C673.txt'){
+    res.statusCode = 200;
+    res.write('D35CF45D0702B41E4D83DB8274C2207291429A3E comodoca.com');
+    res.end();
+  } else {
+    next();
+  }
+});
+
 if (_.contains([true, 'true'], process.env.ALLOW_BOTS)){
   robots.addLine('User-agent: *\nDisallow: /create/');
 } else {
