79a0647e0de5aa9b80a7e850bb4ad9172651b7ea
David <dwanderton@gmail.com>
2016-04-25 18:12:52 +0100

    change tweet default text

--- client/create-context-blocks.js
+++ client/create-context-blocks.js
@@ -345,7 +345,10 @@ Template.create_twitter_section.onCreated(searchTemplateCreatedBoilerplate('twit
 Template.create_twitter_section.onCreated(function(){
   this.addingFunction = function(focusResult, template){
     if(this.tweetstep.get() === 0){
+      
+      // check isCuratorHere
       this.tweetstep.set(1);
+      
     } else if (this.tweetstep.get() === 1){
       var that = this;
       Meteor.setTimeout(function(){ // allow setting of var in click events
