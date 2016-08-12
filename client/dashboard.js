fdba9f7a1cd53ddee6584bdf19011282c41f689d
David <dwanderton@gmail.com>
2016-08-12 13:26:27 -0400

    add three charts

--- client/dashboard.js
+++ client/dashboard.js
@@ -9,32 +9,69 @@ Template.dashboard.onCreated(function(){
   });
   
   Keen.ready(function(){
-    var query = new Keen.Query("count_unique", {
-      eventCollection: "View stream",
-      filters: [
-      {
-          "operator": "not_contains",
-          "property_name": "ip_geo_info.city",
-          "property_value": "null"
-      }
-      ],
-      interval: "daily",
-      targetProperty: "ip_address",
-      timeframe: "this_5_days",
-      timezone: "UTC"
-    });
+    var query = new Keen.Query("count",{
+    eventCollection: "watch page rendered",
+    filters: [
+    {
+        "operator": "not_contains",
+        "property_name": "currentRouteName",
+        "property_value": "curate"
+    }
+],
+    interval: "daily",
+    timeframe: "this_30_days",
+    timezone: "UTC"
+  });
     
-      var query2 = new Keen.Query("count_unique", {
-        eventCollection: "View stream",
-        targetProperty: "ip_address",
-        timeframe: "this_100_years",
-        timezone: "UTC"
-      });
+    var query2 = new Keen.Query("count", {
+    eventCollection: "Click context section in list mode",
+    filters: [
+    {
+        "operator": "not_contains",
+        "property_name": "currentRouteName",
+        "property_value": "curate"
+    },
+],
+    interval: "daily",
+    timeframe: "this_30_days",
+    timezone: "UTC"
+  });
+    
+    var query3 = new Keen.Query("count", {
+    eventCollection: "Click mini-stream to set main stream",
+    filters: [
+    {
+        "operator": "not_contains",
+        "property_name": "currentRouteName",
+        "property_value": "curate"
+    }
+],
+    interval: "daily",
+    timeframe: "this_30_days",
+    timezone: "UTC"
+  });
+    
+      client.draw(query3, document.getElementById("chart-03"), {
+      // Custom configuration here
+      chartType: "areachart",
+      title: false,
+      width: "auto",
+      height: "450px",
+      chartOptions: {
+        chartArea: {
+          height: "85%",
+          left: "5%",
+          top: "5%",
+          width: "90%"
+        },
+        isStacked: true
+      }
+    });
   
     client.draw(query2, document.getElementById("chart-02"), {
       // Custom configuration here
-      charType: "metric",
-      title: "Total Uniue Viewers",
+      chartType: "areachart",
+      title: false,
       width: "auto",
       height: "450px",
       chartOptions: {
