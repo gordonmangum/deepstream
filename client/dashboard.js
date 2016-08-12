8b800fc9149cbc7803475f613d0814cefe0d1580
David <dwanderton@gmail.com>
2016-08-12 16:26:15 -0400

    add dashboard stats!

--- client/dashboard.js
+++ client/dashboard.js
@@ -9,98 +9,213 @@ Template.dashboard.onCreated(function(){
   });
   
   Keen.ready(function(){
-    var query = new Keen.Query("count",{
-    eventCollection: "watch page rendered",
-    filters: [
-    {
-        "operator": "not_contains",
-        "property_name": "currentRouteName",
-        "property_value": "curate"
+    var arrayOfKeenCharts= [];
+    drawKeenCharts = function(arrayOfChartObjects){
+      arrayOfChartObjects.forEach(function(val, index, arr){
+        client.draw(val.query, val.element, val.config);
+      });
     }
-],
-    interval: "daily",
-    timeframe: "this_30_days",
-    timezone: "UTC"
-  });
-    
-    var query2 = new Keen.Query("count", {
-    eventCollection: "Click context section in list mode",
-    filters: [
-    {
-        "operator": "not_contains",
-        "property_name": "currentRouteName",
-        "property_value": "curate"
-    },
-],
-    interval: "daily",
-    timeframe: "this_30_days",
-    timezone: "UTC"
-  });
-    
-    var query3 = new Keen.Query("count", {
-    eventCollection: "Click mini-stream to set main stream",
-    filters: [
-    {
-        "operator": "not_contains",
-        "property_name": "currentRouteName",
-        "property_value": "curate"
-    }
-],
-    interval: "daily",
-    timeframe: "this_30_days",
-    timezone: "UTC"
-  });
-    
-      client.draw(query3, document.getElementById("chart-03"), {
-      // Custom configuration here
-      chartType: "areachart",
-      title: false,
-      width: "auto",
-      height: "450px",
-      chartOptions: {
-        chartArea: {
-          height: "85%",
-          left: "5%",
-          top: "5%",
-          width: "90%"
-        },
-        isStacked: true
-      }
-    });
-  
-    client.draw(query2, document.getElementById("chart-02"), {
-      // Custom configuration here
-      chartType: "areachart",
-      title: false,
-      width: "auto",
-      height: "450px",
-      chartOptions: {
-        chartArea: {
-          height: "85%",
-          left: "5%",
-          top: "5%",
-          width: "90%"
-        },
-        isStacked: true
-      }
-    });
+    arrayOfKeenCharts.push({
+      query: 
+        new Keen.Query("count",{
+          eventCollection: "watch page rendered",
+          filters: [
+            {
+              "operator": "not_contains",
+              "property_name": "currentRouteName",
+              "property_value": "curate"
+            }
+          ],
+          timeframe: "this_30_days",
+          timezone: "UTC"
+        }),
+      element: document.getElementById("chart-01"),
+      config: {
+        // Custom configuration here
+        chartType: "metric",
+        colors: ["#108761"],
+        title: "Views",
 
-    client.draw(query, document.getElementById("chart-01"), {
-      // Custom configuration here
-      chartType: "areachart",
-      title: false,
-      width: "auto",
-      height: "450px",
-      chartOptions: {
-        chartArea: {
-          height: "85%",
-          left: "5%",
-          top: "5%",
-          width: "90%"
-        },
-        isStacked: true
+        height: 200,
+        chartOptions: {
+          chartArea: {
+            height: "85%",
+            left: "5%",
+            top: "5%",
+            width: "90%"
+          },
+          isStacked: true
+        }
       }
-    }); 
+    });
+    
+    arrayOfKeenCharts.push({
+      query: 
+        new Keen.Query("count",{
+          eventCollection: "watch page rendered",
+          filters: [
+            {
+              "operator": "not_contains",
+              "property_name": "currentRouteName",
+              "property_value": "curate"
+            }
+          ],
+          timeframe: "this_30_days",
+          interval: "daily",
+          timezone: "UTC"
+        }),
+      element: document.getElementById("chart-02"),
+      config: {
+        // Custom configuration here
+        chartType: "areachart",
+        title: false,
+
+        height: 200,
+        chartOptions: {
+          chartArea: {
+            height: "85%",
+            left: "5%",
+            top: "5%",
+            width: "90%"
+          },
+          isStacked: true
+        }
+      }
+    });
+    arrayOfKeenCharts.push({
+      query: 
+        new Keen.Query("count",{
+          eventCollection: "Click context section in list mode",
+          filters: [
+            {
+              "operator": "not_contains",
+              "property_name": "currentRouteName",
+              "property_value": "curate"
+            },
+          ],
+          timeframe: "this_30_days",
+          timezone: "UTC"
+        }),
+      element: document.getElementById("chart-03"),
+      config: {
+        // Custom configuration here
+        chartType: "metric",
+        colors: ["#e8b93a"],
+        title: "Card Clicks",
+
+        height: 200,
+        chartOptions: {
+          chartArea: {
+            height: "85%",
+            left: "5%",
+            top: "5%",
+            width: "90%"
+          },
+          isStacked: true
+        }
+      }
+    });
+    arrayOfKeenCharts.push({
+      query: 
+        new Keen.Query("count",{
+          eventCollection: "Click context section in list mode",
+          filters: [
+            {
+              "operator": "not_contains",
+              "property_name": "currentRouteName",
+              "property_value": "curate"
+            },
+          ],
+          interval: "daily",
+          timeframe: "this_30_days",
+          timezone: "UTC"
+        }),
+      element: document.getElementById("chart-04"),
+      config: {
+        // Custom configuration here
+        chartType: "areachart",
+        title: false,
+
+        height: 200,
+        chartOptions: {
+          chartArea: {
+            height: "85%",
+            left: "5%",
+            top: "5%",
+            width: "90%"
+          },
+          isStacked: true
+        }
+      }
+    });
+    arrayOfKeenCharts.push({
+      query: 
+        new Keen.Query("count", {
+          eventCollection: "Click mini-stream to set main stream",
+          filters: [
+            {
+              "operator": "not_contains",
+              "property_name": "currentRouteName",
+              "property_value": "curate"
+            }
+          ],
+          timeframe: "this_30_days",
+          timezone: "UTC"
+      }),
+      element: document.getElementById("chart-05"),
+      config: {
+        // Custom configuration here
+        chartType: "metric",
+        colors: ["#00AF98"],
+        title: "Video Clicks",
+        height: 200,
+        chartOptions: {
+          chartArea: {
+            height: "85%",
+            left: "5%",
+            top: "5%",
+            width: "90%"
+          },
+          isStacked: true
+        }
+      }
+    });
+    arrayOfKeenCharts.push({
+      query: 
+        new Keen.Query("count", {
+          eventCollection: "Click mini-stream to set main stream",
+          filters: [
+            {
+              "operator": "not_contains",
+              "property_name": "currentRouteName",
+              "property_value": "curate"
+            }
+          ],
+          interval: "daily",
+          timeframe: "this_30_days",
+          timezone: "UTC"
+      }),
+      element: document.getElementById("chart-06"),
+      config: {
+        // Custom configuration here
+        chartType: "areachart",
+        title: false,
+        height: 200,
+        chartOptions: {
+          chartArea: {
+            height: "85%",
+            left: "5%",
+            top: "5%",
+            width: "90%"
+          },
+          isStacked: true
+        }
+      }
+    });
+    
+    drawKeenCharts(arrayOfKeenCharts);
+
+   
   });
 });
 Template.dashboard.onRendered(function(){});
