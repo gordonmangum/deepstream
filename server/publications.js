d9584a117434bce834180c25dfb1af6f657a6dc8
David <dwanderton@gmail.com>
2016-08-19 20:03:17 -0400

    fix iss shark issue!!

--- server/publications.js
+++ server/publications.js
@@ -9,11 +9,6 @@ Deepstreams._ensureIndex({
   live: 1
 });
 
-//Deepstreams._ensureIndex({
-//  curatorId: 1
-//});
-//
-
 ContextBlocks._ensureIndex({
   streamShortId: 1
 });
@@ -34,13 +29,6 @@ SuggestedContextBlocks._ensureIndex({
 Meteor.users._ensureIndex({
   username: 1
 });
-
-
-//Streams._ensureIndex({
-//  title: "text",
-//  description: "text"
-//});
-
 
 var deepstreamFields = {
   'deleted': 0,
@@ -63,68 +51,14 @@ var contextFields = {
   'searchQuery': 0
 };
 
-//var readStoryFields = {
-//  draftStory: 0,
-//  history: 0,
-//  narrativeRightsReserved: 0,
-//  //savedAt: 0, // used in analytics
-//  //createdAt:0, // used in analytics
-//  everPublished:0,
-//  //deleted: 0, // currently always blank so no need to filter
-//  //deletedAt: 0, // currently always blank so no need to filter
-//  'analytics.shares': 0,
-//  //'contextBlocks.authorId': 0, // used in analytics
-//  //'contextBlocks.storyShortId': 0, // used in analytics
-//  'contextBlocks.storyId': 0,
-//  'contextBlocks.version': 0,
-//  'contextBlocks.savedAt': 0,
-//  'contextBlocks.publishedAt': 0,
-//  'contextBlocks.createdAt': 0,
-//  'contextBlocks.fullDetails': 0,
-//  'contextBlocks.published': 0,
-//  'contextBlocks.everPublished': 0,
-//  'contextBlocks.searchQuery': 0,
-//  'contextBlocks.searchOption': 0
-//};
-
-//var previewStoryFields = {
-//  shortId: 1,
-//  savedAt: 1,
-//  publishedAt: 1,
-//  published: 1,
-//  userPathSegment: 1,
-//  authorId: 1,
-//  authorName: 1,
-//  authorUsername: 1,
-//  //favorited: 1, // will need to add this back in for non-curated stories to use preview
-//  editorsPick: 1,
-//  editorsPickAt: 1,
-//  //'analytics.views': 1,  // will need to add this back in for non-curated stories to use preview
-//  contextBlockTypeCount: 1,
-//  headerImageFormat: 1,
-//  headerImage: 1,
-//  favoritedTotal: 1,
-//  storyPathSegment: 1,
-//  title: 1
-//};
-
-Meteor.publish("deepstreamsOnAir", function(options) {
+Meteor.publish("deepstreamsOnAir", function() {
   this.unblock();
-  return Deepstreams.find({}, {fields: deepstreamFields});
-  /*
-  options = options ? options : {};
-  _.defaults(options, {page: 0});
   return Deepstreams.find({
     onAir: true
   }, {
-    sort: {
-      createdAt: -1
-    },
+    sort: { createdAt: -1 },
     fields: deepstreamFields,
-    skip: options.page * PUB_SIZE,
-    limit: PUB_SIZE
   });
-  */
 });
 
 
@@ -157,42 +91,6 @@ Meteor.publish('deepstreamsForAdmin', function( search, published ) {
   }
   return Deepstreams.find( query, projection );
 });
-/*
-Meteor.publish("deepstreamsForAdmin", function(options) {
-  this.unblock();
-  options = options ? options : {};
-  _.defaults(options, {page: 0});
-
-  return Deepstreams.find({
-    onAir: true
-  }, {
-    sort: {
-      createdAt: -1
-    },
-    fields: deepstreamFields,
-    skip: options.page * PUB_SIZE,
-    limit: PUB_SIZE
-  });
-});
-*/
-
-//Meteor.publish("curatedStoriesPub", function(options) {
-//  this.unblock();
-//  options = options ? options : {};
-//  _.defaults(options, {page: 0});
-//
-//  return Stories.find({
-//    published: true,
-//    editorsPick: true
-//  }, {
-//    fields: options.preview ? previewStoryFields : readStoryFields,
-//    skip: options.page * PUB_SIZE,
-//    sort: {
-//      editorsPickAt: -1
-//    },
-//    limit: PUB_SIZE
-//  });
-//});
 
 Meteor.publish("bestStreams", function() {
   this.unblock();
@@ -266,24 +164,6 @@ Meteor.publish("myDeepstreams", function() {
     fields: deepstreamFields, sort: {createdAt :-1}
   });
 });
-
-//Meteor.publish("curatedStoriesPub", function(options) {
-//  this.unblock();
-//  options = options ? options : {};
-//  _.defaults(options, {page: 0});
-//
-//  return Stories.find({
-//    published: true,
-//    editorsPick: true
-//  }, {
-//    fields: options.preview ? previewStoryFields : readStoryFields,
-//    skip: options.page * PUB_SIZE,
-//    sort: {
-//      editorsPickAt: -1
-//    },
-//    limit: PUB_SIZE
-//  });
-//});
 
 Meteor.publish("minimalUsersPub", function(userIds) {
   this.unblock();
